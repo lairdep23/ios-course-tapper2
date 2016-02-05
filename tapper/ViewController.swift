8ca1767d41f5d64e1745a056b6cb1709c7452738
Evan Laird <Evan@Evans-MacBook-Pro.local>
2016-02-04 21:16:27 -0600

    added tapper files

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -9,17 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
+    //properties
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    
+    // Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var coinButton: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    @IBAction func onCoinTapped(sender: UIButton!){
+        currentTaps++
+        updateTapsLbl()
+        
+        if isGameOver() {
+            restartGame()
+    }
+    }
+    
+    
+ 
+    @IBAction func onPlayButtonPressed(sender: UIButton) {
+    
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            logoImg.hidden = true
+            howManyTapsTxt.hidden = true
+            playButton.hidden = true
+            
+            coinButton.hidden = false
+            tapsLbl.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            updateTapsLbl()
+        }
+    }
+    
+        func restartGame() {
+            maxTaps = 0
+            howManyTapsTxt.text = ""
+            
+            logoImg.hidden = false
+            playButton.hidden = false
+            howManyTapsTxt.hidden = false
+            
+            coinButton.hidden = true
+            tapsLbl.hidden = true
+            
+        }
+        
+    
+    func isGameOver() ->Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateTapsLbl(){
+        tapsLbl.text = "\(currentTaps) Taps"
     }
-
-
 }
+
+
+
+
+
+
 
